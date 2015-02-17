b3fdfafc9121f57750c33727207023a504dd1bc0
aelgammal <amal.elgammal@scss.tcd.ie>
2015-02-17 16:40:54 +0000

    Android APP working!!!
    Only needs visible IPs for the calls to the WS and Servlets

---
+++ SMART-GH-Android/platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+C:\Users\elgammaa\SMART-GH\SMART-GH-Android\platforms\android\ant-build\classes.dex : \
+C:\Users\elgammaa\SMART-GH\SMART-GH-Android\platforms\android\ant-build\classes\com\coolappz\smartghAndroid\BuildConfig.class \
+C:\Users\elgammaa\SMART-GH\SMART-GH-Android\platforms\android\ant-build\classes\com\coolappz\smartghAndroid\CordovaApp.class \
+C:\Users\elgammaa\SMART-GH\SMART-GH-Android\platforms\android\ant-build\classes\com\coolappz\smartghAndroid\R$attr.class \
+C:\Users\elgammaa\SMART-GH\SMART-GH-Android\platforms\android\ant-build\classes\com\coolappz\smartghAndroid\R$drawable.class \
+C:\Users\elgammaa\SMART-GH\SMART-GH-Android\platforms\android\ant-build\classes\com\coolappz\smartghAndroid\R$string.class \
+C:\Users\elgammaa\SMART-GH\SMART-GH-Android\platforms\android\ant-build\classes\com\coolappz\smartghAndroid\R$xml.class \
+C:\Users\elgammaa\SMART-GH\SMART-GH-Android\platforms\android\ant-build\classes\com\coolappz\smartghAndroid\R.class \
+C:\Users\elgammaa\SMART-GH\SMART-GH-Android\platforms\android\ant-build\dexedLibs\classes-b2e156559a76ff99b655f859b4b4becc.jar \
