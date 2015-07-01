1163322fe707af68a53362c9420499103c8f90db
Nicolas Cardozo <ncardozo@vub.ac.be>
2015-07-01 16:11:35 +0200

    Changes to the graphhopper class in order to take into account information from more sensors than noise and pollution
    Signed-off-by: Nicolas Cardozo <ncardozo@vub.ac.be>

--- SMART-GH-Android/platforms/android/ant-build/CordovaApp-release-unsigned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\elgammaa\SMART-GH\SMART-GH-Android\platforms\android\ant-build\CordovaApp-release-unsigned.apk : \
-C:\Users\elgammaa\SMART-GH\SMART-GH-Android\platforms\android\ant-build\CordovaApp.ap_ \
-C:\Users\elgammaa\SMART-GH\SMART-GH-Android\platforms\android\ant-build\classes.dex \
-C:\Users\elgammaa\SMART-GH\SMART-GH-Android\platforms\android\CordovaLib\ant-build\classes.jar \
