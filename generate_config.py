1ea609c3ff5db81ed6e4492dfc6319850ff98cef
Vivek Nallur <vivek.nallur@gmail.com>
2015-07-01 16:10:47 +0100

    Creates a valid config.properties file

--- generate_config.py
+++ generate_config.py
@@ -2,15 +2,15 @@
 
 # import std libraries
 import logging
+import os.path
 
 
 # import third-party libs
-import requests
 from plumbum import cli
 
 # import our modules
-import constants
-import utils
+import configconstants
+
 
 logger = logging.getLogger('smarthopper.config')
 
@@ -52,32 +52,63 @@ class ConfigGenerator(cli.Application):
         """
             The sensors in the city
         """
-        self._sensors = sensors
+        self._sensors = sensors.split(",")
 
-    @cli.switch(["--transport"], str, list=True)
+    @cli.switch(["--modes"], str)
     def modes(self, modes):
         """
             The modes of transport available with this city
         """
         self._modes = modes
 
-	def create_main_config():
-	"""
-		Creates the main config file called config.properties
-	"""
-		with open("config-pre.heredoc", r) as pre_file:
-			config_pre = pre_file.read()
+    def create_main_config(self):
+        """
+        Creates the main config file called config.properties
+        """
+        map_extension = ".osm"
+        map_file = ''.join([self._city, map_extension])
+        logger.debug("Map file: %s"%(map_file))
 
-		with open("config-post.heredoc", r) as post_file:
-			config_post = post_file.read()
+        osm_map_var = 'osmMap='
+        osm_map_var = ''.join([osm_map_var,map_file])
+        logger.debug("osm_map_var = %s"%(osm_map_var))
 
-		
+        osm_way_var = 'osmreader.acceptWay='
+        osm_way_var = ''.join([osm_way_var,self._modes])
+        logger.debug("osm_way_var = %s"%(osm_way_var))
 
-	def create_city_config():
-	"""
-		Creates the city config file named as <city>.config, where <city> is the value of self._city
-	"""
-		pass
+        with open("config-pre.heredoc", "r") as pre_file:
+                config_pre = pre_file.read()
+
+        with open("config-post.heredoc", "r") as post_file:
+                config_post = post_file.read()
+
+        with open("config.properties", "w") as config_file:
+                config_file.write(config_pre)
+                config_file.write(osm_map_var)
+                config_file.write("\n\n")
+                config_file.write(osm_way_var)
+                config_file.write("\n\n")
+                config_file.write(config_post)
+
+    def create_city_config(self):
+        """
+        Creates the city config file named as <city>.config, where <city> is the value of self._city
+        """
+        subdir = "sensors-config-files"
+        config_extension = '.config'
+        config_file = ''.join([self._city, config_extension])
+
+        city_var = 'CITY_PREFIX = '
+        city_var = ''.join([city_var, self._city])
+
+	logger.debug("sensors found: %s"%(self._sensors))
+	
+	
+
+        
+        
+        
 
     def main(self):
         if not self._is_logging:
@@ -86,7 +117,11 @@ class ConfigGenerator(cli.Application):
             logger.setLevel(logging.DEBUG)
             logger.info("No logger defined. Logging to console...")
 
-	
+        if self._city:
+                logger.debug("Found city: %s"%(self._city))
+                self.create_main_config()
+		self.create_city_config()
+        
             
 
 
